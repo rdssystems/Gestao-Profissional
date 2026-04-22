a5742738fd9cd750e678f3971aec844ee88bf548
Klisman rDs <rds.systems90@gmail.com>
2026-04-22 11:26:25 -0300

    fix: resolve second migration conflict in cursos app (0022_merge)

---
+++ cursos/migrations/0022_merge_20260409_0025.py
@@ -0,0 +1,7 @@
+from django.db import migrations
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('cursos', '0021_chamada_motivo_falta_chamada_motivo_falta_outro_and_more'),
+    ]
+    operations = []
